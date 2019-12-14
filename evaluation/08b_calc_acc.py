f05408cedf17d20fd8160fd1ba530c5892d060f2
MSchnei <marian.schneider@maastrichtuniversity.nl>
2019-12-14 17:51:32 +0000

    Calculate accuracy and f1 score.

--- evaluation/08b_calc_acc.py
+++ evaluation/08b_calc_acc.py
@@ -5,6 +5,8 @@
 import os
 import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
+from tabulate import tabulate
 from io_nii_data import func_nii_to_tf_tensor
 
 # %% Set paramaters
@@ -15,26 +17,91 @@ str_parent_path = str(os.environ['parent_path'])
 # set subject
 str_subj = 'sub-02'
 
+# path to ground truth images
+str_gt_path = os.path.join(str_parent_path, 'data', 'segmentation_data',
+                           'data_mprage')
+# path to predicted segmentations
+str_sg_path = os.path.join(str_parent_path, 'MPRAGEsingle', 'predicted_nii')
+
+# list the segmentation models that should be considered
+lst_sg_models = ['project32_32strides_maxpool_tranposed_classic_pre',
+                 'project43_32strides_maxpool_tranposed_dense_dr_0p05']
+# list header descriptions for every model
+lst_model_headers = ['UNet', 'Tiramisu']
+
+# list the strides (test-time augmentation rates that should be considered)
+lst_aug_strides = ['stride_64', 'stride_32', 'stride_16']
+
 # set path to ground truth
-str_gt = os.path.join(str_parent_path, 'data', 'segmentation_data',
-                      'data_mprage', 'derivatives', str_subj, 'labeled',
+str_gt = os.path.join(str_gt_path, 'derivatives', str_subj, 'labeled',
                       str_subj + '_labels_v01.nii.gz')
 
-# set path to predicted segmentation
-str_ps = os.path.join(str_parent_path, 'MPRAGEsingle', 'predicted_nii',
-                      'project34_32strides_maxpool_tranposed_dense_pre',
-                      'pred_per_class.nii.gz')
+# %%
 
-# %% Step 1: Load nii and convert to tensors
+# Prepare lists for different metrics
+resultsAcc = []
+resultsF1 = []
 
 # Load ground truth image
 tns_gt = func_nii_to_tf_tensor(str_gt, dtype=np.int32)
-# Load predicted segmentation
-# expects one channel in last dimension for every label in gt
-tns_ps = func_nii_to_tf_tensor(str_ps, dtype=np.float32)
 
-# %% Step 2: Calculate accuracy
+# loop over models
+for model_name, sg_model in zip(lst_model_headers, lst_sg_models):
+    
+    # Append model name to lists for row header
+    resultsAcc.append([model_name])
+    resultsF1.append([model_name])
+    
+    # loop over augmentations
+    for ind_aug, aug_stride in enumerate(lst_aug_strides):
 
-m = tf.keras.metrics.SparseCategoricalAccuracy()
-m.update_state(tns_gt, tns_ps)
-var_acc = m.result().numpy()
+        # derive path to predicted segmentations
+        str_ps = os.path.join(str_sg_path, sg_model, aug_stride,
+                              'pred.nii.gz')
+        
+        # derive path to predicted probabilities
+        str_pp = os.path.join(str_sg_path, sg_model, aug_stride,
+                              'pred_per_class.nii.gz')
+
+        # Load nifti files and convert to tensors
+        # Load predicted segmentation
+        tns_ps = func_nii_to_tf_tensor(str_ps, dtype=np.int32)
+        # Load predicted probabilities
+        tns_pp = func_nii_to_tf_tensor(str_pp, dtype=np.float32)
+
+        # Calculate accuracy
+        obj_acc = tf.keras.metrics.SparseCategoricalAccuracy()
+        obj_acc.update_state(tns_gt, tns_pp)
+        var_acc = obj_acc.result().numpy()
+        
+        # Convert to one-hot tensors
+        tns_gt_hot = tf.one_hot(tns_gt, 8, dtype=tns_gt.dtype)
+        tns_ps_hot = tf.one_hot(tns_ps, 8, dtype=tns_ps.dtype)
+        # Calculate f1 score, set average None or 'weighted'
+        obj_f1 = tfa.metrics.F1Score(num_classes=8, average='weighted')
+        obj_f1.update_state(tf.reshape(tns_gt_hot, [-1, 8]),
+                            tf.reshape(tns_ps_hot, [-1, 8]))
+        var_f1 = obj_f1.result().numpy()
+        
+        # Append calculated metrics to metrics list
+        resultsAcc[ind_aug].extend(["%.4f" % var_acc])
+        resultsF1[ind_aug].extend(["%.4f" % var_f1])
+
+
+# %% print tables
+
+print('Accuracy')
+table = tabulate(resultsAcc,  tablefmt='orgtbl',
+                 headers=[''] + lst_aug_strides)
+print(table)
+table = tabulate(resultsAcc,  tablefmt='latex',
+                 headers=[''] + lst_aug_strides)
+print(table)
+
+print('Weighted F1 Score')
+table = tabulate(resultsF1,  tablefmt='orgtbl',
+                 headers=[''] + lst_aug_strides)
+print(table)
+table = tabulate(resultsF1,  tablefmt='latex',
+                 headers=[''] + lst_aug_strides)
+print(table)
